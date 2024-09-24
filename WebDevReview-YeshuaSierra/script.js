ebbe68a29ab42f1e71c82c76c9ba92ece0a96db5
Yeshua B. Sierra <135649679+YeshuaBS-Official@users.noreply.github.com>
2024-09-24 12:20:34 -0400

    Add files via upload

---
+++ WebDevReview-YeshuaSierra/script.js
@@ -0,0 +1,7 @@
+document.getElementById('changeTextButton').addEventListener('click', function() {
+    document.getElementById('dynamicText').innerHTML = 'The text has been changed! Yippy :)';
+});
+
+document.getElementById('changeParagraphButton').addEventListener('click', function(){
+    document.getElementById('paragraph1').innerHTML = 'The text has been changed! Yippy :)';
+});
